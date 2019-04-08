4d9504b5ec713b9367ad6f60960f885014c1b6eb
775371 <44507768+775371@users.noreply.github.com>
2019-04-08 18:38:19 -0400

    Update CT.c

--- src/CT.c
+++ src/CT.c
@@ -272,7 +272,7 @@ CTss(int n, double *y[], double *value,  double *con_mean, double *tr_mean,
         //double n = sizeof(X) / sizeof(*X);  // rows
 	double m = sizeof(X[0]) / sizeof(*X[0]);  // columns
 
-	double** X = matrix(n, m);  // inputs
+	//double** X = matrix(n, m);  // inputs
 	//double** z = matrix(n, 1);  // outputs
 
 	
